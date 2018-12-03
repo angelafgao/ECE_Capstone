7e7df8117b768de481c2d1b290816c47714d83f9
abhutan1 <abhutan1@andrew.cmu.edu>
2018-12-03 12:45:46 -0500

    add downsample results

---
+++ test_eye_detection/downsample_results.py
@@ -0,0 +1,4 @@
+rectangles[[(370, 336) (680, 646)]]
+rectangles[[(178, 178) (327, 327)]]
+rectangles[[(93, 85) (165, 157)]]
+(['Closed', 'Closed', 'Closed', 'Eyes: Failed Detection. Try Repositioning the Camera'], [12.920491933822632, 11.826365947723389, 11.695183038711548, 11.605498790740967])
